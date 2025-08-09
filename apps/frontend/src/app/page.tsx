c423efb6d1968fdeaea8d8aad4fadbe0022a2166
jungyuya <jungyuya@gmail.com>
2025-08-09 23:14:14 +0900

    그라라라라라

--- apps/frontend/src/app/page.tsx
+++ apps/frontend/src/app/page.tsx
@@ -5,7 +5,7 @@ export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-center bg-gray-50 p-4">
       <div className="mb-8 text-center">
-        <h1 className="text-4xl font-bold">NEW 땅고르기 진행중</h1>
+        <h1 className="text-4xl font-bold">NEW 그라라라라라라</h1>
         <p className="text-lg text-gray-600 mt-2">BFF 패턴을 사용, 커스텀 인증 구현, 신기술도입</p>
       </div>
       <SignUp />
