8812e28c2f4e2e997c2d7ce99e4f62e3906c1814
jungyuya <jungyuya@gmail.com>
2025-08-11 08:14:55 +0900

    목숨을걸어라3 - 롤백

--- apps/frontend/src/app/page.tsx
+++ apps/frontend/src/app/page.tsx
@@ -5,7 +5,7 @@ export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-center bg-gray-50 p-4">
       <div className="mb-8 text-center">
-        <h1 className="text-4xl font-bold">니시시시시시시시</h1>
+        <h1 className="text-4xl font-bold">어서오세요 반갑습니다. 키시시시시시싯시</h1>
         <p className="text-lg text-gray-600 mt-2">Turborepo Cache, Speed TEST중</p>
       </div>
       <SignUp />
