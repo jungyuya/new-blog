b5dc1becab17d54946c61705e9cbfb12ba7f3d32
jungyuya <jungyuya@gmail.com>
2025-08-12 01:20:44 +0900

    deploy 분리

--- apps/frontend/src/app/page.tsx
+++ apps/frontend/src/app/page.tsx
@@ -5,8 +5,8 @@ export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-center bg-gray-50 p-4">
       <div className="mb-8 text-center">
-        <h1 className="text-4xl font-bold">어서오세요 반갑습니다. 키시시시시시싯시</h1>
-        <p className="text-lg text-gray-600 mt-2">Turborepo Cache, Speed TEST중</p>
+        <h1 className="text-4xl font-bold">Blog Deep Dive!</h1>
+        <p className="text-lg text-gray-600 mt-2">Self Hosted Runner, Turborepo Cache, Speed TEST중</p>
       </div>
       <SignUp />
     </main>
