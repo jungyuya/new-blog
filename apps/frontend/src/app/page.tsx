b3f1a35ffbfd465e5f3527897d5759b65b0ecf25
jungyuya <jungyuya@gmail.com>
2025-08-13 11:27:02 +0900

    LAst

--- apps/frontend/src/app/page.tsx
+++ apps/frontend/src/app/page.tsx
@@ -1,11 +1,11 @@
-// apps/frontend/src/app/page.tsx (SignUp 폼 추가)
+// apps/frontend/src/app/page.tsx (SignUp 폼추가)
 import SignUp from '@/components/SignUp'; // SignUp 컴포넌트를 import 합니다.
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-center bg-gray-50 p-4">
       <div className="mb-8 text-center">
-        <h1 className="text-4xl font-bold">KEEP GOING && Deep Dive~~!!</h1>
+        <h1 className="text-4xl font-bold">KEEP GOING!</h1>
         <p className="text-lg text-gray-600 mt-2">Docker cache hit!! test, Self Hosted Runner, Speed TEST</p>
       </div>
       <SignUp />
