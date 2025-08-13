c91130df8f77986b442db6b7b8a6aadbe4f16b1c
jungyuya <jungyuya@gmail.com>
2025-08-13 19:19:01 +0900

    빈 이미지 생성 fixed

--- apps/frontend/src/app/page.tsx
+++ apps/frontend/src/app/page.tsx
@@ -5,7 +5,7 @@ export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-center bg-gray-50 p-4">
       <div className="mb-8 text-center">
-        <h1 className="text-4xl font-bold">KEEP GOING && Deep Dive~!</h1>
+        <h1 className="text-4xl font-bold">!!!!</h1>
         <p className="text-lg text-gray-600 mt-2">Docker cache hit!! test, Self Hosted Runner, Speed TEST</p>
       </div>
       <SignUp />
