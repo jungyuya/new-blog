768c65da4b2d608f05e64a520a6f91876b6f5bb3
jungyuya <jungyuya@gmail.com>
2025-08-14 09:27:12 +0900

    Turborepo 캐시 확인

--- apps/frontend/src/app/page.tsx
+++ apps/frontend/src/app/page.tsx
@@ -5,7 +5,7 @@ export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-center bg-gray-50 p-4">
       <div className="mb-8 text-center">
-        <h1 className="text-4xl font-bold">DEEP DIVE</h1>
+        <h1 className="text-4xl font-bold">DEEP DIVE && NEW WORLD?!</h1>
         <p className="text-lg text-gray-600 mt-2">Docker cache hit!! test, Self Hosted Runner, Speed TEST.</p>
       </div>
       <SignUp />
